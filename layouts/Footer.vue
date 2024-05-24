c04f20ad14370f9b6f382f30022a041b82c9b93a
Rémy Canal <remy.canal@epitech.eu>
2024-05-25 01:48:47 +0200

    [+ fix rounded contacts] 25/05/24

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -7,11 +7,11 @@
 				&ldquo; Simplicity is the ultimate sophistication. &rdquo;
 				<span class="text-xl">Leonardo da Vinci</span>
 			</div>
-			<div class="flex justify-center gap-x-6 md:gap-x-10">
+			<div class="flex justify-center gap-x-5 md:gap-x-10">
 				<a
 					href="tel:+330619258299"
 					class="hover-scale-effect group z-10 bg-primary rounded-full border border-secondary-purple hover:border-primary transition duration-300 hover:shadow-around hover:shadow-primary p-4">
-					<Icon name="uil:phone" color="white" class="w-6 h-6 md:w-8 md:h-8 transition-transform transform-gpu group-hover:scale-75 duration-500" />
+					<Icon name="uil:phone" color="white" class="w-8 h-8 transition-transform transform-gpu group-hover:scale-75 duration-500" />
 				</a>
 				<a
 					href="mailto:remy.canal@epitech.eu"
@@ -19,21 +19,21 @@
 					<Icon
 						name="material-symbols:mail-outline-rounded"
 						color="white"
-						class="w-6 h-6 md:w-8 md:h-8 transition-transform transform-gpu group-hover:scale-75 duration-500" />
+						class="w-8 h-8 transition-transform transform-gpu group-hover:scale-75 duration-500" />
 				</a>
 				<a
 					href="https://github.com/remycnl"
 					target="_blank"
                     rel="noopener noreferrer"
 					class="hover-scale-effect group z-10 bg-primary rounded-full border border-secondary-purple hover:border-primary transition duration-300 hover:shadow-around hover:shadow-primary p-4">
-					<Icon name="uil:github" color="white" class="w-6 h-6 md:w-8 md:h-8 transition-transform transform-gpu group-hover:scale-75 duration-500" />
+					<Icon name="uil:github" color="white" class="w-8 h-8 transition-transform transform-gpu group-hover:scale-75 duration-500" />
 				</a>
                 <a
 					href="https://www.linkedin.com/in/remy-canal"
 					target="_blank"
                     rel="noopener noreferrer"
 					class="hover-scale-effect group z-10 bg-primary rounded-full border border-secondary-purple hover:border-primary transition duration-300 hover:shadow-around hover:shadow-primary p-4">
-					<Icon name="uil:linkedin" color="white" class="w-6 h-6 md:w-8 md:h-8 transition-transform transform-gpu group-hover:scale-75 duration-500" />
+					<Icon name="uil:linkedin" color="white" class="w-8 h-8 transition-transform transform-gpu group-hover:scale-75 duration-500" />
 				</a>
 			</div>
 			<div class="flex flex-col justify-center text-center">
