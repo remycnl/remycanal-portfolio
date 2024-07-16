58c5516ae58fcfb6927ca546de11d1b45bfc0921
Rémy Canal <remy.canal@epitech.eu>
2024-07-16 23:20:25 +0200

    [update nuxt.config] 16/07/2024

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   css: [
     '~/assets/css/main.css',
     '~/assets/css/button.css',
     '~/assets/css/valueCard.css',
     '~/assets/css/mouseEffect.css',
   ],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   modules: [
     ['@nuxtjs/google-fonts', {
         families: {
@@ -23,8 +26,11 @@ export default defineNuxtConfig({
     }],
     'nuxt-icon',
   ],
+
   plugins: [
     { src: '~/plugins/gsap.js', ssr: false },
     { src: '~/plugins/global.js', ssr: false },
   ],
+
+  compatibilityDate: '2024-07-16',
 })
