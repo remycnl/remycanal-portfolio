ad4a1a3bcd14c4a7c8489a4b717801c8b05335f5
Rémy Canal <remy.canal@epitech.eu>
2024-08-27 01:21:43 +0200

    [reponsive - Bento] 27/08/2024

--- pages/index.vue
+++ pages/index.vue
@@ -98,16 +98,16 @@
 					class="flex flex-col gap-y-20 justify-center items-center w-full mt-20 lg:mt-32 2xl:mt-40 h-fit">
 					<div class="bento grid gap-5 w-full h-full">
 						<article
-							class="hover:saturate-200 relative overflow-hidden reveal shadow-around p-7 shadow-black-dark backdrop-blur-md bg-secondary-transparent transition-all duration-[1s]">
+							class="article hover:saturate-200 relative overflow-hidden rounded-[1rem] shadow-around p-7 shadow-black-dark backdrop-blur-md bg-secondary-transparent transition-all duration-[1s]">
 							<div
-								class="flex flex-row items-center lg:gap-x-7 2xl:gap-x-10 justify-between h-full w-full font-bold text-white">
+								class="flex flex-row items-center gap-x-7 2xl:gap-x-10 justify-between h-full w-full font-bold text-white">
 								<p
 									id="experience"
-									class="font-[Orbitron] pt-10 lg:text-[8rem] 2xl:text-[15rem]">
+									class="font-[Orbitron] pt-10 text-[10rem] lg:text-[8rem] 2xl:text-[15rem]">
 									0
 								</p>
 								<div
-									class="lg:pt-20 2xl:pt-40 flex flex-col w-full h-full justify-center items-start lg:text-xl 2xl:text-3xl">
+									class="pt-24 lg:pt-20 2xl:pt-40 flex flex-col w-full h-full justify-center items-start text-[1.5rem] md:text-xl 2xl:text-3xl">
 									<p class="font-[Orbitron]">years of</p>
 									<p class="font-[Orbitron]">experience</p>
 								</div>
@@ -116,43 +116,44 @@
 								class="absolute border-[1.25rem] border-black top-[-40%] right-[-40%] h-[80%] w-[80%] rounded-full bg-secondary transition-colors duration-[1s]"></div>
 						</article>
 						<article
-							class="hover:saturate-200 reveal shadow-around shadow-black-dark backdrop-blur-md bg-secondary transition-all duration-[1s]">
+							class="article hover:saturate-200 rounded-[1rem] shadow-around shadow-black-dark backdrop-blur-md bg-secondary transition-all duration-[1s]">
 							<div
 								class="flex flex-col items-center justify-center h-full w-full p-5 font-bold text-primary">
-								<p class="lg:text-[1.2rem] 2xl:text-[1.5rem] font-[Orbitron]">
+								<p
+									class="text-[1.5rem] lg:text-[1.2rem] 2xl:text-[1.5rem] font-[Orbitron]">
 									Developer
 								</p>
 								<p
-									class="lg:text-[2rem] 2xl:text-[3rem] leading-tight uppercase">
+									class="text-[3rem] lg:text-[2rem] 2xl:text-[3rem] leading-tight uppercase text-nowrap">
 									&ldquo;Full-Stack&rdquo;
 								</p>
 							</div>
 						</article>
 						<article
-							class="hover:saturate-200 reveal shadow-around shadow-black-dark backdrop-blur-md bg-secondary transition-all duration-[1s]">
+							class="article hover:saturate-200 rounded-[1rem] shadow-around shadow-black-dark backdrop-blur-md bg-secondary transition-all duration-[1s]">
 							<div
 								class="flex flex-col items-center justify-center h-full w-full p-5 font-bold text-primary">
-								<p class="lg:text-[1.5rem] 2xl:text-[2rem] font-[Orbitron]">
+								<p class="text-[1.5rem] 2xl:text-[2rem] font-[Orbitron]">
 									Student at
 								</p>
-								<p
-									class="lg:text-[3rem] 2xl:text-[4rem] leading-tight uppercase">
+								<p class="text-[3rem] 2xl:text-[4rem] leading-tight uppercase">
 									{Epitech}
 								</p>
 							</div>
 						</article>
 						<article
-							class="hover:saturate-200 reveal shadow-around shadow-black-dark backdrop-blur-md bg-secondary-transparent transition-all duration-[1s]">
+							class="article hover:saturate-200 rounded-[1rem] shadow-around shadow-black-dark backdrop-blur-md bg-secondary-transparent transition-all duration-[1s]">
 							<div
-								class="flex flex-col items-center justify-center h-full w-full p-7 font-bold text-white lg:text-xl 2xl:text-4xl">
+								class="flex flex-col items-center justify-center h-full w-full px-7 lg:p-7 font-bold text-white text-3xl lg:text-xl 2xl:text-4xl">
 								<div
 									class="flex flex-col gap-y-1 items-start justify-center h-full w-full">
 									<p class="font-[Orbitron]">Contributed</p>
 									<p class="font-[Orbitron]">to</p>
 								</div>
 								<div
-									class="flex flex-col justify-center lg:gap-y-7 2xl:gap-y-10 w-full h-full items-end">
-									<p class="lg:text-[5rem] 2xl:text-[8rem] font-[Orbitron]">
+									class="flex flex-col justify-center gap-y-8 lg:gap-y-7 2xl:gap-y-10 w-full h-full items-end">
+									<p
+										class="text-[6rem] lg:text-[5rem] 2xl:text-[8rem] font-[Orbitron]">
 										10
 									</p>
 									<p class="font-[Orbitron]">websites</p>
@@ -160,111 +161,129 @@
 							</div>
 						</article>
 						<article
-							class="relative group overflow-hidden hover:saturate-200 reveal shadow-around shadow-black-dark backdrop-blur-md bg-secondary-transparent transition-all duration-[1s]">
+							class="article relative pb-44 md:pb-0 group overflow-hidden hover:saturate-200 rounded-[1rem] shadow-around shadow-black-dark backdrop-blur-md bg-secondary-transparent transition-all duration-[1s]">
 							<div
-								class="w-full h-full gap-x-5 flex flex-row justify-between items-center px-7 py-10 lg:text-sm 2xl:text-lg text-secondary transition-colors duration-[1s]">
+								class="w-full h-full gap-x-5 gap-y-16 flex flex-col md:flex-row justify-between items-center px-7 py-10 text-lg md:text-sm 2xl:text-lg text-secondary transition-colors duration-[1s]">
 								<div
-									class="w-1/4 h-full flex flex-col gap-y-20 justify-between items-start text-start">
-									<div class="flex flex-col gap-y-3">
+									class="md:hidden w-full h-full flex justify-center items-center">
+									<p
+										class="font-[Orbitron] font-bold text-sm text-center tracking-widest text-white">
+										Some of my<span class="text-[4rem] leading-[4rem]"
+											>Values</span
+										>
+									</p>
+								</div>
+								<div
+									class="w-full md:w-1/4 h-full flex flex-col gap-y-16 md:gap-y-20 justify-between items-center md:items-start text-start">
+									<div class="flex flex-col items-center h-[3.5rem] md:h-auto gap-y-3">
 										<transition name="fade-translate-up" mode="out-in">
 											<div
 												v-if="!isValueHover1"
 												@mouseenter="isValueHover1 = true"
+												@click="isValueHover1 = true"
 												class="flex flex-row gap-x-3 justify-start items-center"
 												key="icon-text">
 												<Icon
 													name="game-icons:small-fire"
 													color="var(--primary-color)"
-													class="lg:w-14 2xl:w-20 h-auto transition-colors duration-[1s]" />
+													class="w-14 2xl:w-20 h-auto transition-colors duration-[1s]" />
 												<p class="font-[Orbitron] font-bold uppercase">
-													Simplicity Sophistication
+													Simplicity <br class="md:hidden">Sophistication
 												</p>
 											</div>
 											<p
 												v-else
 												@mouseleave="isValueHover1 = false"
-												class="text-center text-white"
+												@click="isValueHover1 = false"
+												class="text-center text-white w-3/4 md:w-full"
 												key="description">
 												For a smooth user's experience and clean codebase
 											</p>
 										</transition>
 									</div>
-									<div class="flex flex-col gap-y-3">
+									<div class="flex flex-col items-center h-[3.5rem] md:h-auto gap-y-3">
 										<transition name="fade-translate-down" mode="out-in">
 											<div
 												v-if="!isValueHover2"
 												@mouseenter="isValueHover2 = true"
-												class="flex flex-row gap-x-3 justify-start items-center"
+												@click="isValueHover2 = true"
+												class="flex flex-row-reverse md:flex-row gap-x-3 justify-start items-center text-end md:text-start"
 												key="icon-text">
 												<Icon
 													name="game-icons:rocket"
 													color="var(--primary-color)"
-													class="lg:w-14 2xl:w-20 h-auto transition-colors duration-[1s]" />
+													class="w-14 2xl:w-20 h-auto transition-colors duration-[1s]" />
 												<p class="font-[Orbitron] font-bold uppercase">
-													Innovative Meticulous
+													Innovative <br class="md:hidden">Meticulous
 												</p>
 											</div>
 											<p
 												v-else
 												@mouseleave="isValueHover2 = false"
-												class="text-center text-white"
+												@click="isValueHover2 = false"
+												class="text-center text-white w-3/4 md:w-full"
 												key="description">
 												Engineered with creativity and attention to detail
 											</p>
 										</transition>
 									</div>
 								</div>
-								<div class="w-1/3 h-full flex justify-center items-start">
+								<div
+									class="hidden w-1/3 h-full md:flex justify-center items-start">
 									<p
-										class="font-[Orbitron] font-bold lg:text-sm 2xl:text-lg text-center tracking-widest text-white">
+										class="font-[Orbitron] font-bold text-sm 2xl:text-lg text-center tracking-widest text-white">
 										Some of my<span
-											class="lg:text-[3rem] 2xl:text-[5rem] lg:leading-[3rem] 2xl:leading-[5rem]"
+											class="text-[3rem] 2xl:text-[5rem] leading-[3rem] 2xl:leading-[5rem]"
 											>Values</span
 										>
 									</p>
 								</div>
 								<div
-									class="w-1/4 h-full flex flex-col gap-y-20 justify-between items-end text-end">
-									<div class="flex flex-col gap-y-3">
+									class="w-full md:w-1/4 h-full flex flex-col gap-y-16 md:gap-y-20 justify-between items-center md:items-end text-end">
+									<div class="flex flex-col items-center h-[3.5rem] md:h-auto gap-y-3">
 										<transition name="fade-translate-up" mode="out-in">
 											<div
-												class="flex flex-row gap-x-3 justify-end items-center"
+												class="flex flex-row-reverse md:flex-row gap-x-3 justify-end items-center text-start md:text-end"
 												v-if="!isValueHover3"
-												@mouseenter="isValueHover3 = true">
+												@mouseenter="isValueHover3 = true"
+												@click="isValueHover3 = true">
 												<p class="font-[Orbitron] font-bold uppercase">
-													Autonomous Listening
+													Autonomous <br class="md:hidden">Listening
 												</p>
 												<Icon
 													name="game-icons:suckered-tentacle"
 													color="var(--primary-color)"
-													class="lg:w-14 2xl:w-20 h-auto transition-colors duration-[1s]" />
+													class="w-14 2xl:w-20 h-auto transition-colors duration-[1s]" />
 											</div>
 											<p
 												v-else
 												@mouseleave="isValueHover3 = false"
-												class="text-center text-white">
+												@click="isValueHover3 = false"
+												class="text-center text-white w-3/4 md:w-full">
 												Independently developed with a focus on user feedback
 											</p>
 										</transition>
 									</div>
-									<div class="flex flex-col gap-y-3">
+									<div class="flex flex-col items-center h-[3.5rem] md:h-auto gap-y-3">
 										<transition name="fade-translate-down" mode="out-in">
 											<div
 												class="flex flex-row gap-x-3 justify-end items-center"
 												v-if="!isValueHover4"
-												@mouseenter="isValueHover4 = true">
+												@mouseenter="isValueHover4 = true"
+												@click="isValueHover4 = true">
 												<p class="font-[Orbitron] font-bold uppercase">
-													Open-minded Passionate
+													Open-minded <br class="md:hidden">Passionate
 												</p>
 												<Icon
 													name="game-icons:techno-heart"
 													color="var(--primary-color)"
-													class="lg:w-14 2xl:w-20 h-auto transition-colors duration-[1s]" />
+													class="w-14 2xl:w-20 h-auto transition-colors duration-[1s]" />
 											</div>
 											<p
 												v-else
 												@mouseleave="isValueHover4 = false"
-												class="text-center text-white">
+												@click="isValueHover4 = false"
+												class="text-center text-white w-3/4 md:w-full">
 												Crafted with an open heart and driven by passion
 											</p>
 										</transition>
@@ -277,7 +296,7 @@
 									left: 50%;
 									transform: translateX(-50%);
 								"
-								class="absolute h-[20rem] lg:w-[17rem] 2xl:w-[30rem] rounded-full bg-secondary transition-colors duration-[1s] border-[1.25rem] border-black"></div>
+								class="absolute h-[20rem] w-[17rem] 2xl:w-[30rem] rounded-full bg-secondary transition-colors duration-[1s] border-[1.25rem] border-black"></div>
 						</article>
 					</div>
 					<a
@@ -482,7 +501,7 @@
 					</div>
 					<!-- Desktop -->
 					<div
-						class="hidden lg:flex relative overflow-visible mt-40 justify-between gap-x-28 items-start">
+						class="hidden lg:flex relative overflow-visible mt-40 justify-between gap-x-20 2xl:gap-x-28 items-start">
 						<Icon
 							name="game-icons:wind-slap"
 							color="var(--secondary-color)"
@@ -555,7 +574,7 @@
 										<div
 											class="flex flex-row gap-x-4 items-center justify-start">
 											<h4
-												class="text-gray-light text-[1.5rem] leading-[2rem] tracking-wider font-bold group-hover:text-secondary group-hover:tracking-widest origin-center transform transition-all duration-500 py-5">
+												class="text-gray-light text-nowrap text-[1.5rem] leading-[2rem] tracking-wider font-bold group-hover:text-secondary group-hover:tracking-widest origin-center transform transition-all duration-500 py-5">
 												{{ project.title }}
 											</h4>
 											<Icon
