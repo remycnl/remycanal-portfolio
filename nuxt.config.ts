36d36a009184dc09dd4e88c2cd29218bf2593f38
Rémy Canal <remy.canal@epitech.eu>
2024-10-03 17:39:04 +0200

    [bug build]

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,10 +8,6 @@ export default defineNuxtConfig({
     },
   },
 
-  nitro: {
-    preset: 'vercel', // Assurez-vous d'utiliser le preset Vercel
-  },
-
   css: [
     '@/assets/css/main.css',
     '@/assets/css/button.css',
