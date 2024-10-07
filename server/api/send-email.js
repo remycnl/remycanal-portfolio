50f07642cf77e93d383cf786086bf20bf30909de
Rémy Canal <remy.canal@epitech.eu>
2024-10-07 22:02:24 +0200

    [node 20]

--- server/api/send-email.js
+++ server/api/send-email.js
@@ -1,38 +1,38 @@
-// import nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
 
-// export default defineEventHandler(async (event) => {
-// 	const body = await readBody(event);
+export default defineEventHandler(async (event) => {
+	const body = await readBody(event);
 
-// 	const transporter = nodemailer.createTransport({
-// 		service: "gmail",
-// 		auth: {
-// 			user: process.env.GMAIL_USER,
-// 			pass: process.env.GMAIL_PASS,
-// 		},
-// 	});
+	const transporter = nodemailer.createTransport({
+		service: "gmail",
+		auth: {
+			user: process.env.GMAIL_USER,
+			pass: process.env.GMAIL_PASS,
+		},
+	});
 
-// 	const mailOptions = {
-// 		from: body.email,
-// 		to: process.env.GMAIL_USER,
-// 		subject: `Message from ${body.firstname} ${body.lastname}`,
-// 		text: `
-// Vous avez reçu un nouveau message depuis votre Portfolio :
+	const mailOptions = {
+		from: body.email,
+		to: process.env.GMAIL_USER,
+		subject: `Message from ${body.firstname} ${body.lastname}`,
+		text: `
+Vous avez reçu un nouveau message depuis votre Portfolio :
 
-// ------------------------------------------------------
-// Prénom : ${body.firstname}
-// Nom : ${body.lastname}
-// Email : ${body.email}
-// Entreprise : ${body.company || 'Non spécifié'}
-// ------------------------------------------------------
+------------------------------------------------------
+Prénom : ${body.firstname}
+Nom : ${body.lastname}
+Email : ${body.email}
+Entreprise : ${body.company || 'Non spécifié'}
+------------------------------------------------------
 
-// ${body.message}
-// `,
-// 	};
-// 	try {
-// 		await transporter.sendMail(mailOptions);
-// 		return { status: "success" };
-// 	} catch (error) {
-// 		console.error("Erreur lors de l'envoi de l'email:", error);
-// 		return { status: "error", message: error.message };
-// 	}
-// });
+${body.message}
+`,
+	};
+	try {
+		await transporter.sendMail(mailOptions);
+		return { status: "success" };
+	} catch (error) {
+		console.error("Erreur lors de l'envoi de l'email:", error);
+		return { status: "error", message: error.message };
+	}
+});
