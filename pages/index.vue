f5a0acadfe2f358e1e9d30394674207d9e9eee1f
Rémy Canal <remy.canal@epitech.eu>
2024-10-11 00:02:39 +0200

    [fix] size bento

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
 	<div
 		class="bg-gradient-to-b lg:rounded-t-[4rem] overflow-hidden shadow-around shadow-black from-black via-primary to-black">
 		<div class="bg-dot-primary lg:mx-20 2xl:mx-0 hover:brightness-100">
-			<section id="section-1" class="container mx-4 lg:mx-auto pt-28 lg:pt-40 pb-48">
+			<section id="section-1" class="container mx-auto px-4 lg:px-0 pt-28 lg:pt-40 pb-48">
 				<AboutMe
 					:selectedColor="selectedColor"
 					@update-color="updateSelectedColor" />
