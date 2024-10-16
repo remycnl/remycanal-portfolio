bbaf91f7fded86d7e8b16765657f05fcd2c5050a
Rémy Canal <remy.canal@epitech.eu>
2024-10-16 16:58:04 +0200

    [fix] contact form

--- server/api/send-email.js
+++ server/api/send-email.js
@@ -2,30 +2,35 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default defineEventHandler(async (event) => {
-	const body = await readBody(event);
+export default async function handler(req, res) {
+	if (req.method === "POST") {
+		const body = req.body;
 
-	const mailOptions = {
-		from: process.env.GMAIL_USER,
-		to: process.env.GMAIL_USER,
-		subject: `Message from ${body.firstname} ${body.lastname}`,
-		html: `
-		<p>Vous avez reçu un nouveau message depuis votre Portfolio :</p>
-		<p>------------------------------------------------------</p>
-		<p><strong>Prénom :</strong> ${body.firstname}</p>
-		<p><strong>Nom :</strong> ${body.lastname}</p>
-		<p><strong>Email :</strong> ${body.email}</p>
-		<p><strong>Entreprise :</strong> ${body.company || "Non spécifié"}</p>
-		<p>------------------------------------------------------</p>
-		<p>${body.message}</p>
-    `,
-	};
+		const mailOptions = {
+			from: process.env.GMAIL_USER,
+			to: process.env.GMAIL_USER,
+			subject: `Message from ${body.firstname} ${body.lastname}`,
+			html: `
+					<p>Vous avez reçu un nouveau message depuis votre Portfolio :</p>
+					<p>------------------------------------------------------</p>
+					<p><strong>Prénom :</strong> ${body.firstname}</p>
+					<p><strong>Nom :</strong> ${body.lastname}</p>
+					<p><strong>Email :</strong> ${body.email}</p>
+					<p><strong>Entreprise :</strong> ${body.company || "Non spécifié"}</p>
+					<p>------------------------------------------------------</p>
+					<p>${body.message}</p>
+				`,
+		};
 
-	try {
-		await resend.emails.send(mailOptions);
-		return { status: "success" };
-	} catch (error) {
-		console.error("Erreur lors de l'envoi de l'email:", error);
-		return { status: "error", message: error.message };
+		try {
+			await resend.emails.send(mailOptions);
+			res.status(200).json({ status: "success" });
+		} catch (error) {
+			console.error("Erreur lors de l'envoi de l'email:", error);
+			res.status(500).json({ status: "error", message: error.message });
+		}
+	} else {
+		res.setHeader("Allow", ["POST"]);
+		res.status(405).end(`Method ${req.method} Not Allowed`);
 	}
-});
+}
