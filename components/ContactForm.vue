030b2ae58cfa85d25623e1a8cdca0314bb65deab
Rémy Canal <remy.canal@epitech.eu>
2024-10-16 17:25:32 +0200

    [fix] contact form

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -420,7 +420,7 @@ const submitForm = async () => {
 	};
 
 	try {
-		const response = await fetch("/api/send-email", {
+		const response = await fetch("https://www.remycanal.me/api/send-emails", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
