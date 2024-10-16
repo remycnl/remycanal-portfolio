ad4fc737cc824f1832954b41201e2c5bb6ec735b
Rémy Canal <remy.canal@epitech.eu>
2024-10-16 18:16:40 +0200

    [fix] OPTIONS method

--- api/send-emails.js
+++ api/send-emails.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default async function handler(req, res) {
     res.setHeader("Access-Control-Allow-Origin", "https://www.remycanal.me");
-    res.setHeader("Access-Control-Allow-Methods", "POST");
+    res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
     if (req.method === "OPTIONS") {
