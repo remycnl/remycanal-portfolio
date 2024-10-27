60bcf6bab72cdeef4b7eb313cc4405fff63e8b93
Rémy Canal <remy.canal@epitech.eu>
2024-10-27 17:21:19 +0100

    [fix] size texts

--- components/Projects.vue
+++ components/Projects.vue
@@ -142,7 +142,7 @@
 						:alt="`Logo - ${project.logo}`"
 						preset="portfolioImage"
 						loading="lazy"
-						class="absolute top-4 right-[-10%] scale-50 group-hover:scale-100 opacity-0 group-hover:opacity-10 w-auto h-60 transition-all duration-[0.8s]" />
+						class="absolute top-4 right-[-10%] scale-50 group-hover:scale-100 opacity-0 group-hover:opacity-10 w-auto h-60 transition-all duration-[0.8s] pointer-events-none" />
 					<div
 						class="text-secondary group w-full h-full flex flex-col justify-between items-start">
 						<div
--- components/Skills.vue
+++ components/Skills.vue
@@ -94,13 +94,13 @@
 				preset="portfolioImage"
 				loading="lazy"
 				alt="Click on bubbles to see details (mobile)"
-				class="hover:saturate-200 transition-all duration-500 absolute scale-50 2xl:scale-[.4] top-[-8rem] md:top-[-15rem] lg:top-[-23rem] 2xl:top-[-30.5rem] right-[30%] md:right-[10%] lg:right-[35%] 2xl:right-[40%] z-10" />
+				class="hover:saturate-200 transition-all duration-500 absolute scale-50 2xl:scale-[.4] top-[-8rem] md:top-[-10rem] lg:top-[-23rem] 2xl:top-[-30.5rem] right-[30%] md:right-[45%] lg:right-[35%] 2xl:right-[40%] 3xl:right-[60%] z-10" />
 			<NuxtImg
 				:src="`https://www.remycanal.me/img/stars-info-${selectedColor}.png`"
 				preset="portfolioImage"
 				loading="lazy"
 				alt="Stars = Proficiency level"
-				class="hover:saturate-200 transition-all duration-500 absolute scale-50 2xl:scale-[.4] top-[-8rem] md:top-[-15rem] lg:top-[-24rem] 2xl:top-[-30.5rem] left-[30%] md:left-[30%] lg:left-[30%] z-10" />
+				class="hover:saturate-200 transition-all duration-500 absolute scale-50 2xl:scale-[.4] top-[-8rem] md:top-[-10rem] lg:top-[-24rem] 2xl:top-[-30.5rem] left-[30%] md:left-[35%] lg:left-[30%] z-10" />
 			<BubbleIcon
 				v-for="(skill, index) in skills"
 				class="rounded-full lg:scale-90 2xl:scale-100"
